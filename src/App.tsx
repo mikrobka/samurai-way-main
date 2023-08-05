c03127bcb9426566f8ad1b516a6d4708b54c75d3
Andrey Paramonov <mikrobka231@gmail.com>
2023-08-05 13:25:43 +0300

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
 import {Route, withRouter} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from "./components/Users/UsersContainer";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import {AppStateType} from "./redux/store";
 import {Preloader} from "./components/common/Preloader/Preloader";
 import Login from "./components/Login/Login";
+
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'))
 
 
 type PropsType = MapDispatchToPropsType & MapStateToPropsType
@@ -48,14 +50,15 @@ class App extends Component<PropsType> {
                     <HeaderContainer/>
                     <Navbar/>
                     <div className="app-wrapper-content">
-                        {/*// пас ренднрит нам страницу как только видет в аресной строке требуемый урл */}
-                        <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-                        <Route path={`/profile/:userId?`} render={() => <ProfileContainer/>}/>
-                        <Route path="/users" render={() => <UsersContainer/>}/>
-                        <Route path="/news" render={() => <News/>}/>
-                        <Route path="/music" render={() => <Music/>}/>
-                        <Route path="/settings" render={() => <Settings/>}/>
-                        <Route path="/login" render={() => <Login/>}/>
+                        <Suspense fallback={<Preloader/>}>
+                            <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                            <Route path={`/profile/:userId?`} render={() => <ProfileContainer/>}/>
+                            <Route path="/users" render={() => <UsersContainer/>}/>
+                            <Route path="/news" render={() => <News/>}/>
+                            <Route path="/music" render={() => <Music/>}/>
+                            <Route path="/settings" render={() => <Settings/>}/>
+                            <Route path="/login" render={() => <Login/>}/>
+                        </Suspense>
                     </div>
                 </div>
             );
